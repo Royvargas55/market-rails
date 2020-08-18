1bbb5b07944b84e2ff892374f06d1bba32f4d4c6
Royvargas55 <Royvargas55@gmail.com>
2020-08-18 09:20:41 -0600

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# leonel = User.new(
-#     username: "leonelvega", 
-#     email: "leonelvegavargas@hotmail.com", 
-#     password: "leonelvega", 
-#     password_confirmation: "leonelvega", 
-#     role: 1) 
-# leonel.save!
+leonel = User.new(
+    username: "leonelvega", 
+    email: "leonelvegavargas@hotmail.com", 
+    password: "leonelvega", 
+    password_confirmation: "leonelvega", 
+    role: 1) 
+leonel.save!
 roy = User.new(
     username: "royvargas", 
     email: "royvargas55@gmail.com", 
@@ -25,6 +25,6 @@ alex = User.new(
     password: "alex123", 
     password_confirmation: "alex123") 
 alex.save!
-# Category.create( amount: "1 y 100 mil") 
-# Category.create( amount: "100 mil y 1 millón") 
-# Category.create( amount: "1 millón o más") 
+Category.create( amount: "1 y 100 mil") 
+Category.create( amount: "100 mil y 1 millón") 
+Category.create( amount: "1 millón o más") 
